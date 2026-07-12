d9e522f2937e763ab5319a37565566b344f0a337
DeltachangeOG <66188483+DeltachangeOG@users.noreply.github.com>
2026-07-12 06:50:25 -0500

    Use ceiling division for clock resolution (#17330)
    To ensure divide by zero doesn't happen.

--- src/script/cpp_api/s_security.cpp
+++ src/script/cpp_api/s_security.cpp
@@ -1153,7 +1153,9 @@ int ScriptApiSecurity::sl_os_setlocale(lua_State *L)
 int ScriptApiSecurity::sl_os_clock(lua_State *L)
 {
 	auto t = clock();
-	t = t - t % (SSCSM_CLOCK_RESOLUTION_US * CLOCKS_PER_SEC / 1'000'000);
+	constexpr clock_t clock_resolution =
+		(SSCSM_CLOCK_RESOLUTION_US * CLOCKS_PER_SEC + 1'000'000 - 1) / 1'000'000;
+	t = t - t % clock_resolution;
 	lua_pushnumber(L, static_cast<lua_Number>(t) / static_cast<lua_Number>(CLOCKS_PER_SEC));
 	return 1;
 }
