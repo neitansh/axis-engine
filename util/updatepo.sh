756c1ad4185839d0574ed5c91395f47d6b579d8e
Neitan <kirillgovik@gmail.com>
2026-09-09 00:05:08 +0300

    i18n(ru): перевод догнал переименование и закрыл пробелы в геймпаде и сети

--- util/updatepo.sh
+++ util/updatepo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Update/create luanti po files
+# Update/create axis po files
 
 # an auxiliary function to abort processing with an optional error
 # message
@@ -47,9 +47,9 @@ cd ..
 # First thing first, update the .pot template. We place it in the po/
 # directory at the top level. You a recent enough xgettext that supports
 # --package-name
-potfile=po/luanti.pot
+potfile=po/axis.pot
 echo "updating pot"
-xgettext --package-name=luanti \
+xgettext --package-name=axis \
 	--add-comments='TRANSLATORS:'\
 	--sort-by-file \
 	--add-location=file \
@@ -72,7 +72,7 @@ xgettext --package-name=luanti \
 # Now iterate on all languages and create the po file if missing, or update it
 # if it exists already
 for lang in $langs ; do # note the missing quotes around $langs
-	pofile=po/$lang/luanti.po
+	pofile=po/$lang/axis.po
 	if test -e $pofile; then
 		echo "[$lang]: updating strings"
 		# Drop old strings *before* updating such that they can be re-used
